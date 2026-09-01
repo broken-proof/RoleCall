a3ea07660ecfde4bdea2e0a03be0b68fb17614c5
Nimo Liu <nimo.r.liu@gmail.com>
2026-09-01 19:52:46 -0400

    backend configured with frontend (pom build)

---
+++ backend/src/main/java/com/newbyproof/rolecall/BackendApplication.java
@@ -0,0 +1,13 @@
+package com.newbyproof.rolecall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/newbyproof/rolecall/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newbyproof.rolecall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
