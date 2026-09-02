98ae9af0d259df00964560da359a6db4406dc13d
Nimo Liu <nimo.r.liu@gmail.com>
2026-09-02 11:30:28 -0400

    test stuff

---
+++ backend/src/main/java/com/newbyproof/rolecall/entity/Test.java
@@ -0,0 +1,11 @@
+package com.newbyproof.rolecall.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Test {
+    @Id
+    private String name;
+}
